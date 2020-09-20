00543db24d6eb4e59ba6f3c2fa342e2abb554aac
ObedBalogun <b.obed@yahoo.com>
2020-09-20 23:26:02 +0100

    Update on hosts

--- multimeteor/settings.py
+++ multimeteor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 # from distutils.command.config import config
 
-import django_heroku
+# import django_heroku
 
 from pathlib import Path
 import os
@@ -143,4 +143,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # prod_db  =  dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
