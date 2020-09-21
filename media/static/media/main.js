ded839e002f5facf4cafbed93276e81801ce5453
ObedBalogun <b.obed@yahoo.com>
2020-09-21 19:21:20 +0100

    Side Menu update

---
+++ media/static/media/main.js
@@ -0,0 +1,54 @@
+var fullHeight = function() {
+
+		$('#nav-div').css('height', $(window).height());
+		$(window).resize(function(){
+			$('#nav-div').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+var burgerMenu = function() {
+
+		$('.nav-toggle-js').on('click', function(event){
+			event.preventDefault();
+			var $this = $(this);
+
+			if ($('body').hasClass('panel')) {
+				$this.removeClass('active');
+				$('body').removeClass('panel');
+			} else {
+				$this.addClass('active');
+				$('body').addClass('panel');
+			}
+		});
+	};
+	burgerMenu();
+
+var mobileMenuOutsideClick = function() {
+
+		$(document).click(function (e) {
+	    var container = $("#nav-div, .nav-toggle-js");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+
+	    	if ( $('body').hasClass('panel') ) {
+
+    			$('body').removeClass('panel');
+    			$('.nav-toggle-js').removeClass('active');
+
+	    	}
+
+	    }
+		});
+
+		$(window).scroll(function(){
+			if ( $('body').hasClass('offcanvas') ) {
+
+    			$('body').removeClass('offcanvas');
+    			$('.js-colorlib-nav-toggle').removeClass('active');
+
+	    	}
+		});
+
+	};
+	mobileMenuOutsideClick();
