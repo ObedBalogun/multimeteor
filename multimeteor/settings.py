5cde698569147a96d3e2bf8b71e882ffc5e74170
ObedBalogun <b.obed@yahoo.com>
2020-09-22 09:57:13 +0100

    Side Menu update

--- multimeteor/settings.py
+++ multimeteor/settings.py
@@ -186,6 +186,7 @@ LOGGING = {
         },
     }
 }
+DEBUG_PROPAGATE_EXCEPTIONS = True
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
