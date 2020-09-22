ce223d0d014aaea7b4ea57d2075b274b8eb42985
ObedBalogun <b.obed@yahoo.com>
2020-09-22 20:47:36 +0100

    Side Menu update

--- multimeteor/settings.py
+++ multimeteor/settings.py
@@ -144,14 +144,14 @@ STATICFILES_STORAGE = 'multimeteor.storage.CompressedManifestStaticFilesStorage'
 AWS_ACCESS_KEY_ID = 'AKIAXPED6J37KD24XJF3'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'bincom'
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 AWS_S3_OBJECT_PARAMETERS ={
     'CacheControl':'max-age=86400'
 }
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+# PUBLIC_MEDIA_LOCATION = 'media'
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
