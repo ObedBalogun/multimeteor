d1d09c08f91d2f19a0b159da252a3ee1e78e8300
ObedBalogun <b.obed@yahoo.com>
2020-09-22 10:33:57 +0100

    Side Menu update

--- multimeteor/settings.py
+++ multimeteor/settings.py
@@ -133,11 +133,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media/static'),
 ]
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'multimeteor.storage.CompressedManifestStaticFilesStorage'
 
 #
 
