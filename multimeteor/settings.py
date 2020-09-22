2cdebf34ac00040c3770d3d33ee24d4055b3abb5
ObedBalogun <b.obed@yahoo.com>
2020-09-22 10:25:35 +0100

    Side Menu update

--- multimeteor/settings.py
+++ multimeteor/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
