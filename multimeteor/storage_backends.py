0a4739af8bf5a7a786a81620ec7e42cd91275770
ObedBalogun <b.obed@yahoo.com>
2020-09-22 10:45:41 +0100

    Side Menu update

--- multimeteor/storage_backends.py
+++ multimeteor/storage_backends.py
@@ -1,5 +1,5 @@
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto import S3Boto3Storage
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = 'False'
