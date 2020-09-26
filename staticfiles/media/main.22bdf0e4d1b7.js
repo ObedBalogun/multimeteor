0deb827e0cc2ae3470c1ce35e54b4f5ac8866371
ObedBalogun <b.obed@yahoo.com>
2020-09-26 12:53:56 +0100

    FancyBox Update

---
+++ staticfiles/media/main.22bdf0e4d1b7.js
@@ -0,0 +1,37 @@
+var fullHeight = function() {
+
+		$('#nav-div').css('height', $(window).height());
+		$(window).resize(function(){
+			$('#nav-div').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+
+var bodyPanel = function () {
+	$('.nav-toggle').click(function(){
+		$(this).toggleClass('open');
+		if ($('body').hasClass('panel')) {
+
+ 				$('body').removeClass('panel');
+ 			} else {
+
+ 				$('body').addClass('panel');
+ 			}
+	});
+}
+bodyPanel();
+
+var imageZoom = function () {
+	$(".zoom").hover(function(){
+
+		$(this).addClass('transition');
+		},
+		function(){
+
+		$(this).removeClass('transition');
+	});
+}
+
+imageZoom();
